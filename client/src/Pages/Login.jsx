bd84fbe1ee1f06b6bbe1f0d8b56e6f52e5a67da2
Yurii <yurii.rudenko26@gmail.com>
2024-02-28 23:12:29 +0200

    added-login-and-registration

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,23 +1,62 @@
 import React, { useEffect } from 'react';
 import {useForm} from 'react-hook-form';
 import './styles/login.css'
+import { login, registration } from '../http/userAPI.js';
 
 const Login = () => {
     const {register, formState: {errors, isValid}, handleSubmit} = useForm();
 
-    const onSubmit = values => console.log(values);
-
     const isLogin = window.location.pathname === '/login';
+
+    const onSubmit = values => {
+
+        let data;
+        if(isLogin) {
+            data = login(values);
+        }
+        else {
+            data = registration(values);
+        }
+        
+    };
 
     return (
         <div className='login'>
             <div className="container">
                 <h2>{isLogin ? 'Login' : 'Registration'}</h2>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <input placeholder='Enter tag or email' type="text" style={errors?.username && {borderColor: "rgb(182, 44, 44)"}} {...register("username", { required: { value: true, message: 'Enter tag or email' }, minLength: { value: 3, message: 'Login must be at least 3 characters' }, maxLength: { value: 16, message: 'Username must not exceed 16 characters' } })} name="username" />
-                    {errors?.username && <p className='login-error'>{errors?.username?.message}</p>}
-                    <input placeholder='Enter password' type="password" style={errors?.password && {borderColor: "rgb(182, 44, 44)"}} {...register("password", { required: { value: true, message: 'Enter password' }, minLength: { value: 3, message: 'Password must be at least 3 characters' }, maxLength: { value: 16, message: 'Password must not exceed 16 characters' } })} name="password" />
-                    {errors?.password && <p className='login-error'>{errors?.password?.message}</p>}
+                {isLogin ? (
+                    <>
+                        <input placeholder='Enter tag or email' type="text" style={errors?.login && {borderColor: "rgb(182, 44, 44)"}} {...register("login", { required: { value: true, message: 'Enter tag or email' }, minLength: { value: 3, message: 'Login must be at least 3 characters' }, maxLength: { value: 16, message: 'Login must not exceed 16 characters' } })} name="login" />
+                        {errors?.login && <p className='login-error'>{errors?.login?.message}</p>}
+                        <input placeholder='Enter password' type="password" style={errors?.password && {borderColor: "rgb(182, 44, 44)"}} {...register("password", { required: { value: true, message: 'Enter password' }, minLength: { value: 3, message: 'Password must be at least 3 characters' }, maxLength: { value: 16, message: 'Password must not exceed 16 characters' } })} name="password" />
+                        {errors?.password && <p className='login-error'>{errors?.password?.message}</p>}
+                    </>
+                ) : (
+                    <>
+                        <input placeholder='Enter your name' type="text" style={errors?.name && {borderColor: "rgb(182, 44, 44)"}} {...register("name", { required: { value: true, message: 'Enter name' }, minLength: { value: 3, message: 'Name must be at least 3 characters' }, maxLength: { value: 20, message: 'Name must not exceed 20 characters' } })} name="name" />
+                        {errors?.name && <p className='login-error'>{errors?.name?.message}</p>}
+
+                        <input placeholder='Enter your email' type="email" style={errors?.email && {borderColor: "rgb(182, 44, 44)"}} {...register("email", { required: { value: true, message: 'Enter email' }, minLength: { value: 4, message: 'Email must be at least 4 characters' }, maxLength: { value: 40, message: 'Email must not exceed 40 characters' } })} name="email" />
+                        {errors?.email && <p className='login-error'>{errors?.email?.message}</p>}
+
+                        <input placeholder='Enter your tag' type="text" style={errors?.tag && {borderColor: "rgb(182, 44, 44)"}} {...register("tag", { required: { value: true, message: 'Enter tag' }, minLength: { value: 3, message: 'Tag must be at least 3 characters' }, maxLength: { value: 16, message: 'Tag must not exceed 16 characters' } })} name="tag" />
+                        {errors?.tag && <p className='login-error'>{errors?.tag?.message}</p>}
+
+                        <input placeholder='Enter your phone' type="text" style={errors?.phone && {borderColor: "rgb(182, 44, 44)"}} {...register("phone", { 
+                            required: { value: true, message: 'Enter phone number' },
+                            pattern: { 
+                                value: /^[0-9]{10}$/, 
+                                message: 'Enter a valid phone number (10 digits)' 
+                            }
+                        })} name="phone" />
+                        {errors?.phone && <p className='login-error'>{errors?.phone?.message}</p>}
+
+                        <input placeholder='Enter password' type="password" style={errors?.password && {borderColor: "rgb(182, 44, 44)"}} {...register("password", { required: { value: true, message: 'Enter password' }, minLength: { value: 6, message: 'Password must be at least 6 characters' }, maxLength: { value: 16, message: 'Password must not exceed 16 characters' } })} name="password" />
+                        {errors?.password && <p className='login-error'>{errors?.password?.message}</p>}
+                        
+                    </>
+                )}
                     <div className="submit-container">
                         <input className='submit' type="submit" value={isLogin ? 'Login' : 'Register'}/>
                     </div>
---
+++ client/src/http/userAPI.js
@@ -0,0 +1,35 @@
+import { $authHost, $host } from "./indexAPI.js";
+import { jwtDecode } from "jwt-decode";
+
+export const registration = async (values) => {
+    try {
+        const {data} = await $host.post('/api/user/registration', values);
+        localStorage.setItem('token', data.token);
+        return jwtDecode(data.token);
+    } catch (error) {
+        alert(error.response.data);
+    }
+
+}
+
+export const login = async (values) => {
+    try {
+        const {data} = await $host.post('/api/user/login', values);
+        localStorage.setItem('token', data.token);
+        return jwtDecode(data.token);
+    } catch (error) {
+        alert(error.response.data);
+    }
+    
+}
+
+export const check = async () => {
+    try {
+        const {data} = await $authHost.get('/api/user/auth');
+        localStorage.setItem('token', data.token);
+        return jwtDecode(data.token);
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
