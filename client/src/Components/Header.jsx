b5da1ab0a88deb0f0f44dcee077d49d2f326a643
Yurii <yurii.rudenko26@gmail.com>
2024-03-08 21:53:58 +0200

    added-create-chat-modal

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,14 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Context } from '..';
 import { observer } from 'mobx-react-lite';
-import { Dropdown } from 'rsuite';
-import 'rsuite/Dropdown/styles/index.css';
+import { Modal, Button, Uploader, Dropdown } from 'rsuite';
+import CameraRetroIcon from '@rsuite/icons/legacy/CameraRetro';
+import 'rsuite/dist/rsuite-no-reset.min.css';
 import menuIcon from '../images/menu.svg';
 import { useNavigate } from 'react-router-dom';
 
 const Header = observer(() => {
     const { user, chat } = useContext(Context);
     const [userApp, setUser] = useState([]);
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);  
 
     const navigate = useNavigate();
 
@@ -30,6 +35,14 @@ const Header = observer(() => {
         );
     };
 
+    const menuButton = (props, ref) => {
+        return (
+            <div {...props} ref={ref} className="menu-button">
+                <img src={menuIcon} alt="MENU" className='menu-icon'/>
+            </div>
+        );
+    }
+
     useEffect(() => {
         if (user.user) {
             setUser(user.user);
@@ -39,8 +52,12 @@ const Header = observer(() => {
     return (
         <div className='header'>
             <div className="left">
-                <img src={menuIcon} alt="MENU" className='menu-icon'/>
-                <h1>Message.app</h1>
+                <Dropdown trigger="click" renderToggle={menuButton}>
+                    <Dropdown.Item style={{fontSize: "20px", color: "#00d2ff"}}>Profile</Dropdown.Item>
+                    <Dropdown.Item onClick={handleOpen} style={{fontSize: "20px"}}>Create chat</Dropdown.Item>
+                    <Dropdown.Item style={{fontSize: "20px", minWidth: "200px"}}>Find chat/user</Dropdown.Item>
+                </Dropdown>
+            <h1>Message.app</h1>
             </div>
             <Dropdown trigger="click" renderToggle={renderButton}>
                 <Dropdown.Item style={{fontSize: "18px"}}>Profile</Dropdown.Item>
@@ -49,7 +66,32 @@ const Header = observer(() => {
                 <Dropdown.Item onClick={logout} style={{fontSize: "18px", color: "Red"}}>Log off</Dropdown.Item>
             </Dropdown>
                 
-
+            <Modal open={open} onClose={handleClose}>
+                <Modal.Header>
+                    <Modal.Title>Create new chat</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="modal-choose-container">
+                            <Uploader listType="picture" action="">
+                                <button>
+                                    <CameraRetroIcon />
+                                </button>
+                            </Uploader>
+                            <div className="right">
+                                <p>Enter name of the chat</p>
+                                <input type="text" className='input-chat-name'/>
+                            </div>
+                        </div>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={handleClose} size="lg" appearance="primary">
+                            Next
+                        </Button>
+                        <Button onClick={handleClose} size="lg" appearance="ghost">
+                            Cancel
+                        </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 })
